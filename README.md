32da9cce54cc0fe56e16c9b4164550baa9639779
Roger Dickey <rdickey@whytespyder.com>
2014-08-30 10:46:10 -0500

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+lead-collector-client
+
+PHP API client to a queue based data collection system.
+
+Requirements:
+* PHP >= 5.4
+* cURL extension for PHP
+
+Installation
+`require vendor/autoload.php`
---
+++ lib/WhyteSpyder/LeadCollector.php
@@ -0,0 +1,15 @@
+<?php
+ /**
+ * Created by Roger Dickey, Jr
+ * rdickey@whytespyder.com
+ * 8/30/14 10:38 AM
+ */
+
+namespace WhyteSpyder;
+
+
+class LeadCollector {
+    public function save($source, $data) {
+        throw new \Exception("Not implemented yet!");
+    }
+} 
