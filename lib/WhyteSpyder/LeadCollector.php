69cd8f34114a7bd16140a00dd7f10bf412276213
Roger Dickey <rdickey@whytespyder.com>
2014-08-30 11:21:06 -0500

    Implement the `save` method, so it actually communicates with a Rackspace queue.

--- lib/WhyteSpyder/LeadCollector.php
+++ lib/WhyteSpyder/LeadCollector.php
@@ -7,9 +7,48 @@
 
 namespace WhyteSpyder;
 
+use OpenCloud\Rackspace;
 
 class LeadCollector {
-    public function save($source, $data) {
-        throw new \Exception("Not implemented yet!");
+    private $username;
+    private $api_key;
+    private $ttl = 300;
+    private $queue_region = 'DFW';
+    private $queue_name = 'ws-lead-collector';
+
+    public function __construct($username, $api_key) {
+        $this->username = $username;
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * @param $source
+     * @param array $data
+     * @throws \Exception
+     */
+    public function save($source, $data = array()) {
+        try {
+            $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+                'username' => $this->username,
+                'apiKey' => $this->api_key,
+            ));
+
+            $service = $client->queuesService(null, $this->queue_region);
+
+            $service->setClientId();
+
+            $queue = $service->getQueue($this->queue_name);
+
+            $queue->createMessage(array(
+                'body' => array(
+                    'source' => $source,
+                    'form' => $data,
+                ),
+                'ttl' => $this->ttl
+            ));
+        }
+        catch (\Exception $e) {
+            throw new Exception(sprintf("Lead Collector: (%d) %s", $e->getCode(), $e->getMessage()));
+        }
     }
 } 
