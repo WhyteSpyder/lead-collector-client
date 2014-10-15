d5de61dde0d964f1eeb66dd210f6bb1b8c305f77
Roger Dickey <rdickey@whytespyder.com>
2014-10-15 07:43:08 -0500

    Don't rely on a single regional queue in case there is a problem. If the first attempt failed, try to send the message to another queue, if the 2nd attempt fails, just send the message details to support in an email.

--- lib/WhyteSpyder/LeadCollector.php
+++ lib/WhyteSpyder/LeadCollector.php
@@ -13,7 +13,6 @@ class LeadCollector {
     private $username;
     private $api_key;
     private $ttl = 300;
-    private $queue_region = 'DFW';
     private $queue_name = 'ws-lead-collector';
 
     public function __construct($username, $api_key) {
@@ -33,7 +32,7 @@ class LeadCollector {
                 'apiKey' => $this->api_key,
             ));
 
-            $service = $client->queuesService(null, $this->queue_region);
+            $service = $client->queuesService(null, 'DFW');
 
             $service->setClientId();
 
@@ -47,8 +46,30 @@ class LeadCollector {
                 'ttl' => $this->ttl
             ));
         }
-        catch (\Exception $e) {
-            throw new \Exception(sprintf("Lead Collector: (%d) %s", $e->getCode(), $e->getMessage()));
+        catch (\Exception $e1) {
+            try {
+                $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+                    'username' => $this->username,
+                    'apiKey' => $this->api_key,
+                ));
+
+                $service = $client->queuesService(null, 'ORD');
+
+                $service->setClientId();
+
+                $queue = $service->getQueue($this->queue_name);
+
+                $queue->createMessage(array(
+                    'body' => array(
+                        'source' => $source,
+                        'form' => $data,
+                    ),
+                    'ttl' => $this->ttl
+                ));
+            }
+            catch (\Exception $e2) {
+                mail('support@whytespyder.com', 'Lead Collector - Failed Queue Connection', json_encode(array('source' => $source, 'form' => $data)), "From: support@whytespyder.com\r\n");
+            }
         }
     }
 } 
